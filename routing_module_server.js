c014afaedacfbe1593c0599d8e92ae160ab0bcb0
David Dokupil <daviddokupil@Kekdig.local>
2024-03-06 21:09:01 -0600

    Home and Login pages, but without functionality

--- routing_module_server.js
+++ routing_module_server.js
@@ -1,22 +1,40 @@
 const express = require("express");
-const api = require("./routes/books.js");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+// const db = require("./db_connection.js").db_connection;
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 const port = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.render(`pages/home`, {page_css: "home.css", page_title: "Reptiles \"r\" Us "});
 });
+
+app.get("/login", function (req, res) {
+  res.render(`pages/login`);
+});
+
+// app.get("/val", (req, res) => {
+//   console.log(req);
+// })
 
 app.get("/unique", function (req, res) {
   res.send("This is a different page!");
 });
 
+// app.get("/homepage", (req, res) => {
+//   res.send("<!DOCTYPE html><head><meta charset=\"UTF-8\"><title>Reptiles \"R\" Us | Home</title><link rel=stylesheet src=\"main.css\"></head><body><table><thead><th>Product 1</th></thead><tbody><tr><td>dim</td></tr><tr><td>weight</td></tr></tbody></table></body>");
+// });
 
-
-app.use("/api", api);
+// app.get("/db", function (req, res) {
+// 	let sql = `SELECT * FROM books`;
+ 
+//    db.query(sql, (err, result) => {
+//        if(err) throw err;
+//       console.log(result);
+//         res.render(`pages/home_partials`, {books: result, page_title: 'DB Connection Example' });
+//     });
+// });
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
